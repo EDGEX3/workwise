8a07aa015253ec060f4627b37ecaa7f7db4475d8
mrsbytehero <himanshu.creator.info@gmail.com>
2024-02-06 23:38:43 +0530

    expanse complite

--- lib/layouts/pageLayout/pages/dashboard/dashboard.dart
+++ lib/layouts/pageLayout/pages/dashboard/dashboard.dart
@@ -2,20 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:workwise/layouts/pageLayout/pages/dashboard/widget/cardsLayout.dart';
 import 'package:workwise/layouts/pageLayout/pages/widget/navbar.dart';
 import 'package:workwise/utils/const/size.dart';
-import 'package:workwise/widgets/listbox/listbox.dart';
+import 'package:workwise/utils/const/text.dart';
+import 'package:workwise/widgets/expanse/expanse.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return Column(
       children: [
-         NavBar(), 
-         SizedBox(height: TSizes.spaceBtwSections,),
-         CardSection(),
-         SizedBox(height: TSizes.spaceBtwSections,),
-        //  ListBox(DataContainer: Text("hi"))
+        NavBar(),
+        SizedBox(
+          height: TSizes.spaceBtwSections,
+        ),
+        CardSection(),
+        SizedBox(
+          height: TSizes.spaceBtwSections,
+        ),
+        Expanse()
       ],
     );
   }
--- lib/utils/const/text.dart
+++ lib/utils/const/text.dart
@@ -10,3 +10,18 @@ TextStyle mid = TextStyle(
   fontWeight: FontWeight.w400,
   letterSpacing: 0.11,
 );
+TextStyle semlg = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0.22,
+);
+TextStyle semmd = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.13,
+);
+TextStyle semsm = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.11,
+);
