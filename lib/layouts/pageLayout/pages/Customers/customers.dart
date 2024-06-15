4ff8739f6653a40f290152fa339143ebd8d1be92
krunalbhadesiya <2101031000089@silveroakuni.ac.in>
2024-06-15 23:10:11 +0530

    Add CustomerList and CustomerInfo widgets

--- lib/layouts/pageLayout/pages/Customers/customers.dart
+++ lib/layouts/pageLayout/pages/Customers/customers.dart
@@ -1,10 +1,121 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:workwise/layouts/pageLayout/pages/widget/navbar/navbar.dart';
+import 'package:workwise/layouts/pageLayout/pages/widget/searchbar/searchbar.dart';
+import 'package:workwise/utils/const/colors.dart';
+import 'package:workwise/utils/const/size.dart';
+import 'package:workwise/widgets/basic/coustomButton.dart';
+import 'package:workwise/widgets/basic/title.dart';
+import 'package:workwise/widgets/customers/customerlist.dart';
 
     class Customers extends StatelessWidget {
       const Customers({super.key});
     
       @override
       Widget build(BuildContext context) {
-        return const Text('Customers');
+        return Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Stack(
+            children: [
+          Padding(
+          padding: const EdgeInsets.only(
+            right: TSizes.spaceBtwSections,
+            left: TSizes.spaceBtwSections,
+            top: TSizes.spaceBtwSections,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const NavBar(),
+              SizedBox(height: TSizes.spaceBtwSections),
+              Expanded(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Searchbar(),
+                      const SizedBox(height: TSizes.spaceBtwSections),
+                      CoustomTitle("Customers"),
+                      const SizedBox(height: TSizes.spaceBtwSections),
+                      Container(
+                        width: double.infinity,
+                        child: coustomButton(
+                          width: double.infinity,
+                          height: 50,
+                          wedget: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Icon(Iconsax.add_square4),
+                              const SizedBox(width: TSizes.defaultSpace),
+                              const Text(
+                                'Add New Customer',
+                                style: TextStyle(
+                                  color: TColors.textSecondary,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  letterSpacing: 0.15,
+                                ),
+                              )
+                            ],
+                          ),
+                          onClick: () {
+                            showSheet(
+                                context,
+                                Column(
+                                  children: [
+                                    Container(
+                                      width: 60,
+                                      height: 5,
+                                      color: TColors.white30,
+                                    ),
+                                    const SizedBox(height: TSizes.spaceBtwSections,),
+                                    const Searchbar()
+                                  ],
+                                ));
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: TSizes.spaceBtwSections),
+                      const CustomerList(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+            ],
+          ),
+        );
       }
     }
+void showSheet(BuildContext ctx, Widget childs) {
+  showMaterialModalBottomSheet(
+    context: ctx,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(TSizes.lg),
+        topRight: Radius.circular(TSizes.lg),
+      ),
+    ),
+    clipBehavior: Clip.antiAlias,
+    backgroundColor: TColors.black10,
+    builder: (BuildContext context) {
+      return Container(
+        width: MediaQuery.of(context).size.width,
+        height: 508,
+        child: ClipRect(
+          child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10), child: Padding(
+            padding: const EdgeInsets.all(TSizes.lg),
+            child: childs,
+          )),
+        ),
+      );
+    },
+  );
+}
