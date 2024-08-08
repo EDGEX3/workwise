293365cecf0ec1629bfdb07bce343b3e5c61bc51
krunalbhadesiya <2101031000089@silveroakuni.ac.in>
2024-08-08 16:37:57 +0530

    Krunal 08-08-2024

---
+++ lib/widgets/billing/BillingForm.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:workwise/utils/const/colors.dart';
+import 'package:workwise/utils/const/size.dart';
+import 'package:workwise/widgets/basic/CustomButton.dart';
+import 'package:workwise/widgets/InputField/InputField.dart';
+
+class BillingForm extends StatelessWidget {
+  const BillingForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        InputField(
+          iconPath: 'assets/icons/Iconsax/twotone/hashtag.svg',
+          labelText: "Product ID",
+          hintText: "EDGP102321",
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems),
+        InputField(
+          iconPath: 'assets/icons/Iconsax/twotone/box-search.svg',
+          labelText: "Product Name",
+          hintText: "Paper Towel",
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems),
+        InputField(
+          iconPath: 'assets/icons/Iconsax/twotone/dollar-circle.svg',
+          labelText: "Rate",
+          hintText: "1000 Rs",
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems),
+        InputField(
+          iconPath: 'assets/icons/Iconsax/twotone/box.svg',
+          labelText: "Qty.",
+          hintText: "8",
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems),
+        InputField(
+          iconPath: 'assets/icons/Iconsax/twotone/discount-shape.svg',
+          labelText: "Discount",
+          hintText: "5%",
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems),
+        InputField(
+          iconPath: 'assets/icons/Iconsax/twotone/percentage-circle.svg',
+          labelText: "Tax",
+          hintText: "5%",
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems),
+        customButton(
+          width: double.infinity,
+          height: TSizes.buttonSize,
+          buttonRadius: TSizes.buttonRadius,
+          boxBorderColor: TColors.white30,
+          color: TColors.primary,
+          wedge: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Add Product',
+                style: TextStyle(
+                  color: TColors.secondary,
+                  fontSize: TSizes.fontSizeSm,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0.15,
+                ),
+              ),
+            ],
+          ),
+          onClick: () {},
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems),
+        customButton(
+          width: double.infinity,
+          height: TSizes.buttonSize,
+          buttonRadius: TSizes.buttonRadius,
+          boxBorderColor: TColors.white30,
+          color: TColors.primary,
+          wedge: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Delete',
+                style: TextStyle(
+                  color: TColors.secondary,
+                  fontSize: TSizes.fontSizeSm,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0.15,
+                ),
+              ),
+            ],
+          ),
+          onClick: () {},
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/widgets/billing/Tag.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:workwise/utils/const/colors.dart';
+import 'package:workwise/utils/const/size.dart';
+
+class Tag extends StatelessWidget {
+  final String title;
+  const Tag({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // height: 20,
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+      clipBehavior: Clip.antiAlias,
+      decoration: ShapeDecoration(
+        // color: TColors.white25,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(width: 1, color: TColors.white30),
+          borderRadius: BorderRadius.circular(TSizes.borderRadiusLg),
+        ),
+      ),
+      child: Text(
+        title,
+        style: TextStyle(
+          color: TColors.secondary,
+          fontSize: TSizes.fontSizeExSm,
+          fontWeight: FontWeight.w400,
+          letterSpacing: 0.09,
+        ),
+      ),
+    );
+  }
+}
